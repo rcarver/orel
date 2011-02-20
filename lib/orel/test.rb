03303e7d4436464878747885dd750e8efa4f4e32
Ryan Carver <ryan@typekit.com>
2011-02-20 12:47:09 -0800

    a test helper to sort output

--- lib/orel/test.rb
+++ lib/orel/test.rb
@@ -1,6 +1,7 @@
 require 'orel'
 require 'active_record'
 require 'mysql2'
+require 'stringio'
 
 Orel.logger = Logger.new(File.dirname(__FILE__) + "/../../log/test.log")
 Orel.logger.info "\n\nBeginning test #{Time.now}\n"
@@ -14,6 +15,7 @@ ActiveRecord::Base.establish_connection(
   :username => 'root',
   :password => ''
 )
+
 
 class ActiveRecord::ConnectionAdapters::Mysql2Adapter
   def primary_keys(table) #:nodoc:
@@ -41,11 +43,37 @@ end
 
 module Orel
   module Test
+
+    # Print '---' before and after the block is executed.
     def self.wrap
       puts '---'
       yield
       puts '---'
     end
+
+    # Print '---' before and after the block is executed,
+    # and also sort anything written to stdout within the
+    # block. This is to deal with the fact that relations
+    # are not ordered, but we need ordered output to
+    # check with simple string comparisons.
+    def self.wrap_and_sort
+      puts '---'
+      out = $stdout
+      str = String.new
+      begin
+        $stdout = StringIO.new(str)
+        yield
+      ensure
+        $stdout = out
+        str.split("\n").sort.each { |line|
+          puts line
+        }
+      end
+      puts '---'
+    end
+
+    # Print '---' before and after the results of
+    # a sql query.
     def self.show(*args)
       wrap {
         Orel.query(*args).each { |row|
