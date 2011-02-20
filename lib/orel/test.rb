0157f36a86449eadae4a6bf6e62f3d1bbd979be0
Ryan Carver <ryan@typekit.com>
2011-02-20 12:53:14 -0800

    don't need these

--- lib/orel/test.rb
+++ lib/orel/test.rb
@@ -16,31 +16,6 @@ ActiveRecord::Base.establish_connection(
   :password => ''
 )
 
-
-class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  def primary_keys(table) #:nodoc:
-    keys = []
-    result = execute("describe #{quote_table_name(table)}")
-    result.each do |h|
-      #keys << h["Field"] if h["Key"] == "PRI"
-      keys << h[0] if h[3] == "PRI"
-    end
-    #result.free
-    keys
-  end
-end
-
-module Arel
-  class Table
-    def primary_keys
-      @primary_keys ||= begin
-        primary_key_names = @engine.connection.primary_keys(name)
-        primary_key_names.map { |k| self[k] }
-      end
-    end
-  end
-end
-
 module Orel
   module Test
 
